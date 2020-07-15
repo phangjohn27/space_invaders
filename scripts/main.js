e5cd2bd35c515f1654408354a6e79849171af065
John Phang <phangjohn1992@gmail.com>
2020-07-15 01:24:49 -0700

    coming up with game engine, don't have settings yet, have the game start by not using all boundaries

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,53 @@ const canvas =  document.getElementById('ufoCanvas');
 canvas.width = 900;
 canvas.height = 750;
 
+//I want to auto-resize my canvas
+function resize(){
+    // Our canvas must cover full height of screen regardless of the resolution
+    const height = window.innerHeight - 20;
+
+    //So we need to calculate the proper scaled width that should work wel with every resolution
+    const ratio = canvas.width / canvas.height
+    const width = height * ratio;
+
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
+}
+
+window.addEventListener('load', resize, false);
+
+// Game Basics
+function GameBasics(canvas){
+    this.canvas = canvas;
+    this.width = canvas.width;
+    this.height = canvas.height;
 
 
+    //active playing field
+    //How Javascript rights function;
+    this.playBoundaries = {
+        top: 150,
+        bottom: 650,
+        left: 100,
+        right: 800
+    };
 
+    this.setting = {
+        //game settings
+    }
 
+    //collect here the different position, states of the game
+    //Stack data structure to use instead of a queue?
+    this.positionContainer = [];
+}
 
+// Return to current game position, status. Always returns the top element of positionContainer.
+GameBasics.prototype.presentPosition = function () {
+    // > 0 means if positionContainer is not empty.
+    //Then return the last position it was at
+    //Obvious if it is empty then return null.
+    return this.positionContainer.length > 0 ? this.positionContainer[this.positionContainer.length  - 1] : null;
+};
 
 
 
